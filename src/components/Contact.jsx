4af04074e74fdf90cf2edce2443048e262c457a2
theMungai <mungaithesimon@gmail.com>
2025-05-28 09:05:47 +0300

    Adds Responsiveness to ContactUsPage component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,22 +4,22 @@ import Inputs from "./common/Inputs.jsx";
 const Contact = () => {
   return (
     <div className="bg-customWhite">
-      <div className="hero relative  mb-[150px] py-10 bg-[url('/images/service-hero.jpg')] bg-cover bg-center min-h-screen grid ">
+      <div className="hero relative  mb-[150px] xs:mb-20 sm:mb-[100px] md:mb-[120px] py-10 bg-[url('/images/service-hero.jpg')] bg-cover bg-center min-h-screen grid ">
         <div className="absolute top-0 inset-0 bg-black/50 z-0"></div>
       </div>
 
-      <div className="container font-dmsans px-[210px]">
-        <h3 className='text-[#282938] text-center font-bold mb-7 text-[20px]'>Get In Touch</h3>
-        <h1 className='text-[#333333] text-[60px] font-extrabold text-center mb-8 '>Contact Us</h1>
-        <p className='mb-[80px] font-normal text-[#555555] text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+      <div className="container font-dmsans px-[210px] xs:p-2 sm:p-6 md:p-10 lg:p-[50px]">
+        <h3 className='text-[#282938] text-center font-bold mb-7 xs:mb-3 sm:mb-4 text-[20px]'>Get In Touch</h3>
+        <h1 className='text-[#333333] text-[60px] xs:text-[32px] sm:text-[36px] md:text-[50px] font-extrabold text-center mb-8 xs:mb-3 sm:mb-4 '>Contact Us</h1>
+        <p className='mb-20 xs:mb-12 sm:mb-14 md:mb-16 font-normal text-[#555555] text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
 
         <form>
-            <div className='flex justify-between items-center mb-8'>
+            <div className='flex justify-between items-center mb-8 xs:flex-col sm:flex-col'>
                 <Inputs placeholder="Enter your last name" label="First name" text="text" basis="[48%]"/>
                 <Inputs placeholder="Enter your first name" label="Last name" text="text" basis="[48%]"/>
             </div>
 
-            <div className='flex justify-between items-center mb-8'>
+            <div className='flex justify-between items-center mb-8 xs:flex-col sm:flex-col'>
                 <Inputs placeholder="Enter your your email" label="Email" text="email" basis="[48%]"/>
                 <Inputs placeholder="Enter your phone number" label="Phone number" text="number" basis="[48%]"/>
             </div>
@@ -45,13 +45,13 @@ const Contact = () => {
 
             <label>
                 <span className='mb-2.5 text-[#282938] font-normal text-[19px] block '>Message</span>
-                <textarea name="message" id="message" required placeholder='Type your nessage' className='min-h-[200px] mb-8 p-4 text-[1rem] text-[#282938] border border-customTealBlue outline-none rounded-[6px] w-full bg-customWhite ' ></textarea>
+                <textarea name="message" id="message" required placeholder='Type your nessage' className='min-h-[200px] xs:min-h-[120px] sm:min-h-[150px] mb-8 p-4 text-[1rem] text-[#282938] border border-customTealBlue outline-none rounded-[6px] w-full bg-customWhite ' ></textarea>
             </label>
             <label className='flex items-center gap-x-4 mb-8'>
                 <input  type="checkbox"  name="accept-terms"  id="accept-terms"  className="w-5 h-5 rounded-[4px] border border-customTealBlue  checked:bg-customTealBlue accent-customTealBlue checked:border-customTealBlue focus:outline-none"/>
                 <span>I accept the terms</span>
             </label>
-            <button className='py-5 mb-[150px] px-[150px] bg-customTealBlue rounded-[8px] font-[500] font-dmsans text-[19px] text-white relative transform -translate-x-1/2 left-1/2'>Submit</button>
+            <button className='py-5 xs:py-3 sm:py-4 mb-mb-[150px] xs:mb-20 sm:mb-[100px] md:mb-[120px]  px-[150px] xs:px-28 sm:px-32 md:px-36 bg-customTealBlue rounded-[8px] font-[500] font-dmsans text-[19px] text-white relative transform -translate-x-1/2 left-1/2'>Submit</button>
         </form>
       </div>
     </div>
--- src/components/common/Inputs.jsx
+++ src/components/common/Inputs.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Inputs = ({label, placeholder, type, basis, name, value, onChange}) => {
   return (
-      <div className={`basis-${basis}`}>
+      <div className={`basis-${basis} xs:w-full sm:w-full`}>
           <label>
               <span className='mb-2.5 text-[#282938] font-normal text-[19px] block '>{label}</span>
-              <input name={name} value={value} onChange={onChange} type={type} required placeholder={placeholder} className='bg-transparent p-4 text-[1rem] text-[#282938] border border-customTealBlue outline-none rounded-[6px] w-full ' />
+              <input name={name} value={value} onChange={onChange} type={type} required placeholder={placeholder} className='bg-transparent p-4 xs:p-3 text-[1rem] xs:text-[0.95rem] text-[#282938] border border-customTealBlue outline-none rounded-[6px] w-full ' />
           </label>
       </div>
   );
