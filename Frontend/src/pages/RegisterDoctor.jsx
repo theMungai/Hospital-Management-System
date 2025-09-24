c3f8d9af92821d605306842c953b7734a59147c8
theMungai <mungaithesimon@gmail.com>
2025-09-24 20:19:39 +0300

    feat: enhance UI in the RegisterDoctor.jsx component

--- Frontend/src/pages/RegisterDoctor.jsx
+++ Frontend/src/pages/RegisterDoctor.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import RegisterForm from "../components/SignUp/Doctor/RegisterForm.jsx";
+import Layout from "../components/Layout.jsx";
 
 function RegisterDoctor() {
     return (
-        <div>
-            <RegisterForm />
-        </div>
+        <Layout>
+
+            <div
+                className="register-container py-8  mt-[140px] xs:mt-20 sm:mt-20 md:mt-20 lg:mt-20 sm:p-6 md:p-10 lg:p-[50px] bg-customWhite min-h-screen flex items-center justify-center">
+                <div
+                    className="md:w-[90%] flex justify-around gap-x-[50px] bg-white min-h-[80vh] w-4/5 xs:w-[95%] sm:w-[90%] rounded-[10px] shadow-[0_10px_25px_rgba(0,_0,_0,_0.1)] overflow-hidden">
+                    <RegisterForm/>
+                </div>
+            </div>
+        </Layout>
     );
 }
 
