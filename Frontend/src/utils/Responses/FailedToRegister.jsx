79d6f44a691c3463a559b5bd87ee98f37c3c2353
theMungai <mungaithesimon@gmail.com>
2025-09-25 13:37:49 +0300

    feat: fix FailedToRegister and add button to go back to SelectPortal.jsx component

--- Frontend/src/utils/Responses/FailedToRegister.jsx
+++ Frontend/src/utils/Responses/FailedToRegister.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
+import {ArrowLongLeftIcon} from "@heroicons/react/24/solid";
+
 import Layout from "../../components/Layout.jsx";
+import {Link} from "react-router-dom";
 
 function FailedToRegister() {
     return (
         <Layout>
-            <div className="grid place-content-center h-screen text-center font-dmsans p-4">
+            <div className=" grid place-content-center h-screen text-center font-dmsans p-4">
                 <div
                     className="bg-[url('/images/error.png')] bg-contain bg-no-repeat bg-center
-                               w-full max-w-[600px] aspect-square mx-auto">
+                               w-full max-w-[700px] aspect-square mx-auto">
                 </div>
 
                 <h3 className="text-red-500 font-bold text-3xl md:text-4xl mt-6">
                     Form submission failed
                 </h3>
-                <p className="text-gray-600 mt-2">
+                <p className="text-gray-600 my-3">
                     Oops! Looks like we’re not fully plugged in. Please try again.
                 </p>
+
+                <Link to={'/sign-up'}>
+                    <button className="relative left-1/2 transform -translate-x-1/2 flex items-center gap-x-2 py-2 px-3.5 w-[130px] border  border-customTealBlue rounded text-customTealBlue
+                ">
+                        <ArrowLongLeftIcon className="h-5 w-5" aria-hidden="true"/>
+                        Go back
+                    </button>
+                </Link>
             </div>
         </Layout>
     );
