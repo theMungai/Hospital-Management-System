e7a431252dcb555c2f5153fd6ce7462dcb44bd8e
theMungai <mungaithesimon@gmail.com>
2025-12-05 23:09:36 +0300

    feat: implement profile_image handling in user creation route and store uploaded images securely

--- Backend/App/routes/user_routes.py
+++ Backend/App/routes/user_routes.py
@@ -1,11 +1,13 @@
-from fastapi import Depends, status, HTTPException, APIRouter
+from fastapi import Depends, status, HTTPException, APIRouter, Form, UploadFile, File
 from sqlalchemy.orm import Session
 from typing import List
 
+from ..core.enums import RoleEnum
 from ..models import User
 from ..database.database import get_db
 from ..utils.hashing import hash_pwd
 from ..schemas.UserSchema import UserOut, UserCreate
+from .doctor_routes import save_file_securely
 
 router = APIRouter(
     tags=['Users']
@@ -29,16 +31,39 @@ def get_user_by_id(id: int, db:Session=Depends(get_db)):
 
 
 @router.post("/users", status_code=status.HTTP_201_CREATED, response_model=UserOut)
-def create_user(user: UserCreate,db: Session=Depends(get_db)):
-    existing_user = db.query(User).filter(User.email == user.email).first()
+def create_user(
+        first_name: str = Form(...),
+        last_name: str = Form(...),
+        email: str = Form(...),
+        phone_number: str = Form(...),
+        password: str = Form(...),
+        role: RoleEnum = Form(...),
+        profile_image: UploadFile = File(...),
+        db: Session = Depends(get_db)
+
+):
+    existing_user = db.query(User).filter(User.email == email).first()
 
     if existing_user:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User with this email already exists.")
 
-    hashed_password = hash_pwd(user.password)
-    user.password = hashed_password
+    hashed_password = hash_pwd(password)
+    password = hashed_password
 
-    new_user = User(**user.dict())
+    image_path = None
+    if profile_image:
+        image_path = save_file_securely(profile_image)
+
+    new_user = User(
+        first_name = first_name,
+        last_name = last_name,
+        email = email,
+        phone_number = phone_number,
+        password = password,
+        role = role,
+        profile_image = image_path
+    )
+
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
